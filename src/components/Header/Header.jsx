80e5d6ebf64dc100417f7f888108dbc7b3593603
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-16 21:22:15 +0530

    all responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import './Header.css'
+import { BiMenuAltRight } from 'react-icons/bi';
+import OutsideClickHandler from "react-outside-click-handler";
+
 
 export const Header = () => {
+    const [menu, setMenu] = React.useState(false);
+
+    const getMenuStyles = (menu) => {
+        if (document.documentElement.clientWidth <= 800) {
+            return { right: !menu && "-100%" };
+        }
+    }
+
+
     return (
         <section className="h-wrapper">
             <div className="flexCenter paddings innerWidth h-container">
                 <img src="./logo.png" alt="logo" width={100} />
-                <div className="flexCenter h-menu">
-                    <a href="">Residencies</a>
-                    <a href="">Our Value</a>
-                    <a href="">Contact Us</a>
-                    <a href="">Get Started</a>
-                    <button className="button">
-                        <a href="">Contact</a>
-                    </button>
-                </div>
+
+                <OutsideClickHandler
+                    onOutsideClick={() => {
+                        setMenu(false)
+                    }}
+                >
+
+                    <div className="flexCenter h-menu"
+                        style={getMenuStyles(menu)}>
+
+                        <a href="">Residencies</a>
+                        <a href="">Our Value</a>
+                        <a href="">Contact Us</a>
+                        <a href="">Get Started</a>
+                        <button className="button">
+                            <a href="">Contact</a>
+                        </button>
+                    </div>
+                     </OutsideClickHandler>
+                    <div className="menu-icon" onClick={() => setMenu((prev) => !prev)}>
+                        <BiMenuAltRight size={30} />
+                    </div>
             </div>
+
         </section>
 
 
