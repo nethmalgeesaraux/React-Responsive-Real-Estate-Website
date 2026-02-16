e2ea7fe237c6832aa94e8b1c2797a111a42bc95b
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-16 21:33:44 +0530

    animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Hero.css'
 import { HiLocationMarker } from 'react-icons/hi'
 import CountUp from 'react-countup'
+import { motion } from 'framer-motion'
+
 
 export const Hero = () => {
   return (
@@ -12,10 +14,17 @@ export const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <dev className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring"
+              }}
+            >
               Discover <br />
               Most Suitable <br /> Property
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart hero-des">
@@ -60,9 +69,17 @@ export const Hero = () => {
         </div>
         {/* {right-section} */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
-            <img src="./hero-image.png" alt="" />
-          </div>
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2
+            }}
+          >
+            <div className="image-container">
+              <img src="./hero-image.png" alt="" />
+            </div>
+          </motion.div>
         </div>
 
       </div>
